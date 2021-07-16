99474323bf1bfd627ef2592a58fddb3a09ad4adb
Libby Lebyane <lebyanelm@nextify.co.za>
2021-07-16 15:48:04 +0200

    added header component to components module

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,12 +1,18 @@
+import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HeaderComponent
   ]
 })
 export class ComponentsModule { }
